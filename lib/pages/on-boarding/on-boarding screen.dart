187edbb6e314d7480afc77257f5601a08edbaab2
Husseinhtm99 <husseinhtm99@gmail.com>
2022-12-14 23:40:18 +0200

    fixed some errors

--- lib/pages/on-boarding/on-boarding screen.dart
+++ lib/pages/on-boarding/on-boarding screen.dart
@@ -24,7 +24,7 @@ class OnBoard extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Spacer(),
               Expanded(
