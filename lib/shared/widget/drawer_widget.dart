062dfc7d27d6b0b2954599f6d544dfc9d5ee5829
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-08-16 07:27:55 +0300

    Update DrawerWidget with ListOfItem and remove unnecessary code✅

--- lib/shared/widget/drawer_widget.dart
+++ lib/shared/widget/drawer_widget.dart
@@ -1,8 +1,6 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:sociality/pages/notifications/notifications_screen.dart';
 import 'package:sociality/pages/password/change_password.dart';
 import 'package:sociality/pages/post/save_post_screen.dart';
@@ -33,281 +31,79 @@ class DrawerWidget extends StatelessWidget {
                     Stack(
                       alignment: AlignmentDirectional.bottomCenter,
                       children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(8.0),
-                              topRight: Radius.circular(8.0),
-                            ).r,
-                          ),
-                          width: double.infinity,
+                        ImageWithShimmer(
+                          imageUrl: cubit.userModel!.cover,
+                          boxFit: BoxFit.fill,
                           height: 200.h,
-                          child: CachedNetworkImage(
-                            imageUrl: cubit.userModel!.cover,
-                            fit: BoxFit.fill,
-                            height: 200.h,
-                            width: double.infinity,
-                            progressIndicatorBuilder:
-                                (context, url, downloadProgress) => Center(
-                              child: AdaptiveIndicator(
-                                os: getOs(),
-                              ),
-                            ),
-                            errorWidget: (context, url, error) => Center(
-                              child: AdaptiveIndicator(
-                                os: getOs(),
-                              ),
-                            ),
-                          ),
+                          width: double.infinity,
                         ),
-                        // CircleAvatar(
-                        //   backgroundColor: Colors.blueAccent,
-                        //   radius: 42.r,
-                        //   child: CircleAvatar(
-                        //     radius: 40.r,
-                        //     child: ClipRRect(
-                        //       borderRadius: BorderRadius.circular(65).r,
-                        //       child: CachedNetworkImage(
-                        //         imageUrl: cubit.userModel!.image,
-                        //         fit: BoxFit.fill,
-                        //         height: 200.h,
-                        //         width: double.infinity,
-                        //         progressIndicatorBuilder:
-                        //             (context, url, downloadProgress) =>
-                        //             Center(
-                        //               child: AdaptiveIndicator(
-                        //                 os: getOs(),
-                        //               ),
-                        //             ),
-                        //         errorWidget: (context, url, error) =>
-                        //             Center(
-                        //               child: AdaptiveIndicator(
-                        //                 os: getOs(),
-                        //               ),
-                        //             ),
-                        //       ),
-                        //     ),
-                        //   ),
-                        // ),
                       ],
                     ),
-                    space(0, 35.h),
-                    SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Text(
-                            cubit.userModel!.name.toUpperCase(),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            textAlign: TextAlign.center,
-                            style: GoogleFonts.roboto(
-                                fontSize: 30.sp,
-                                color:
-                                    cubit.isDark ? Colors.black : Colors.white),
-                          ),
-                          const MyDivider(color: Colors.cyan),
-                          InkWell(
-                            onTap: () {
-                              SocialCubit.get(context).getMyPosts(uId);
-                              SocialCubit.get(context).getFriends(uId!);
-                              navigateTo(context, const MyProfileScreen());
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                      horizontal: 10.0, vertical: 20)
-                                  .r,
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    IconlyBroken.user2,
-                                    size: 30.sp,
-                                    color: cubit.isDark
-                                        ? Colors.black
-                                        : Colors.white,
-                                  ),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  Text(
-                                    'Profile',
-                                    style: GoogleFonts.roboto(
-                                        fontSize: 30,
-                                        color: cubit.isDark
-                                            ? Colors.black
-                                            : Colors.white),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              navigateTo(context, const NotificationScreen());
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                      horizontal: 10.0, vertical: 20)
-                                  .r,
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    IconlyBroken.notification,
-                                    size: 30.sp,
-                                    color: cubit.isDark
-                                        ? Colors.black
-                                        : Colors.white,
-                                  ),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  Text(
-                                    'Notifications',
-                                    style: GoogleFonts.roboto(
-                                        fontSize: 30,
-                                        color: cubit.isDark
-                                            ? Colors.black
-                                            : Colors.white),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              navigateTo(context, const SavePostScreen());
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                      horizontal: 10.0, vertical: 20)
-                                  .r,
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    IconlyBroken.bookmark,
-                                    size: 30.sp,
-                                    color: cubit.isDark
-                                        ? Colors.black
-                                        : Colors.white,
-                                  ),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  Text(
-                                    'Saved Post',
-                                    style: GoogleFonts.roboto(
-                                        fontSize: 30,
-                                        color: cubit.isDark
-                                            ? Colors.black
-                                            : Colors.white),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              navigateTo(context, const EditPasswordScreen());
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                      horizontal: 10.0, vertical: 20)
-                                  .r,
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    IconlyBroken.password,
-                                    size: 30.sp,
-                                    color: cubit.isDark
-                                        ? Colors.black
-                                        : Colors.white,
-                                  ),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  Text(
-                                    'Rest Password',
-                                    style: GoogleFonts.roboto(
-                                        fontSize: 30,
-                                        color: cubit.isDark
-                                            ? Colors.black
-                                            : Colors.white),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              cubit.changeAppMode();
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                      horizontal: 10.0, vertical: 20)
-                                  .r,
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.dark_mode,
-                                    size: 30.sp,
-                                    color: cubit.isDark
-                                        ? Colors.black
-                                        : Colors.white,
-                                  ),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  Text(
-                                    'Theme Mode',
-                                    style: GoogleFonts.roboto(
-                                        fontSize: 30,
-                                        color: cubit.isDark
-                                            ? Colors.black
-                                            : Colors.white),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              logOut(context);
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                      horizontal: 10.0, vertical: 20)
-                                  .r,
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.power_settings_new_rounded,
-                                    size: 30.sp,
-                                    color: cubit.isDark
-                                        ? Colors.black
-                                        : Colors.white,
-                                  ),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  Text(
-                                    'Logout',
-                                    style: GoogleFonts.roboto(
-                                      fontSize: 30.sp,
-                                      color: cubit.isDark
-                                          ? Colors.black
-                                          : Colors.white,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                    SizedBox(height: 35.h),
+                    Text(
+                      cubit.userModel!.name,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20).r,
+                      child: const MyDivider(color: Colors.cyan),
+                    ),
+                    ListOfItem(
+                      function: () {
+                        cubit.getMyPosts(uId);
+                        cubit.getFriends(uId!);
+                        navigateTo(context, const MyProfileScreen());
+                      },
+                      cubit: cubit,
+                      text: 'Profile',
+                      iconData: IconlyBroken.user2,
+                    ),
+                    ListOfItem(
+                      function: () {
+                        navigateTo(context, const NotificationScreen());
+                      },
+                      cubit: cubit,
+                      text: 'Notifications',
+                      iconData: IconlyBroken.notification,
+                    ),
+                    ListOfItem(
+                      function: () {
+                        navigateTo(context, const SavePostScreen());
+                      },
+                      cubit: cubit,
+                      text: 'Saved Post',
+                      iconData: IconlyBroken.bookmark,
+                    ),
+                    ListOfItem(
+                      function: () {
+                        navigateTo(context, const EditPasswordScreen());
+                      },
+                      cubit: cubit,
+                      text: 'Rest Password',
+                      iconData: IconlyBroken.password,
+                    ),
+                    ListOfItem(
+                      function: () {
+                        cubit.changeAppMode();
+                      },
+                      cubit: cubit,
+                      text: 'Theme Mode',
+                      iconData: IconlyBroken.star,
+                    ),
+                    ListOfItem(
+                      function: () {
+                        logOut(context);
+                      },
+                      cubit: cubit,
+                      text: 'Logout',
+                      iconData: IconlyBroken.closeSquare,
                     ),
                   ],
                 ),
                 Positioned(
-                  top: 160.r,
+                  top: 190.r,
                   left: 40.r,
                   right: 40.r,
                   child: CircleAvatar(
@@ -332,6 +128,50 @@ class DrawerWidget extends StatelessWidget {
           : AdaptiveIndicator(
               os: getOs(),
             ),
+    );
+  }
+}
+
+class ListOfItem extends StatelessWidget {
+  const ListOfItem({
+    Key? key,
+    required this.cubit,
+    required this.iconData,
+    required this.text,
+    required this.function,
+  }) : super(key: key);
+
+  final SocialCubit cubit;
+  final IconData iconData;
+  final String text;
+  final void Function()? function;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        function!();
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 20).r,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              iconData,
+              size: 30.sp,
+              color: AppMainColors.greyColor,
+            ),
+            SizedBox(
+              width: 10.w,
+            ),
+            Text(
+              text,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
