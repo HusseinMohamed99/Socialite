139c404d50e6437b7d6f5984fa98f000f236e040
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-08-20 03:15:22 +0300

    Rename CreatePost to CreatePosts.

--- lib/pages/feed/feedscreen.dart
+++ lib/pages/feed/feedscreen.dart
@@ -1,9 +1,6 @@
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:sociality/pages/addPost/add_post_screen.dart';
 import 'package:sociality/shared/components/image_with_shimmer.dart';
 import 'package:sociality/shared/components/indicator.dart';
-import 'package:sociality/shared/components/navigator.dart';
 import 'package:sociality/shared/components/show_toast.dart';
 import 'package:sociality/shared/cubit/socialCubit/social_cubit.dart';
 import 'package:sociality/shared/cubit/socialCubit/social_state.dart';
@@ -14,6 +11,7 @@ import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sociality/shared/styles/color.dart';
 import 'package:sociality/shared/widget/build_post_item.dart';
+import 'package:sociality/shared/widget/create_post.dart';
 import 'package:sociality/shared/widget/story_widget.dart';
 
 class FeedScreen extends StatelessWidget {
@@ -191,87 +189,6 @@ class StoriesItem extends StatelessWidget {
                   itemCount: cubit.stories.length),
             ),
             SizedBox(width: 10.w),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CreatePosts extends StatelessWidget {
-  const CreatePosts({
-    super.key,
-    required this.cubit,
-  });
-
-  final SocialCubit cubit;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      elevation: 10,
-      margin: const EdgeInsets.all(10).r,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0).r,
-        child: Row(
-          children: [
-            CircleAvatar(
-              radius: 25.r,
-              child: ImageWithShimmer(
-                radius: 25.r,
-                imageUrl: cubit.userModel!.image,
-                width: 100.w,
-                height: 100.h,
-                boxFit: BoxFit.fill,
-              ),
-            ),
-            Expanded(
-              child: Container(
-                width: 220.w,
-                height: 35.h,
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                margin: const EdgeInsets.all(10).r,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: AppMainColors.greyColor,
-                  ),
-                  borderRadius: BorderRadius.circular(25).r,
-                ),
-                child: TextButton(
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  style: TextButton.styleFrom(
-                    backgroundColor: AppMainColors.greyColor.withOpacity(0.1),
-                  ),
-                  child: Text(
-                    "' What's on your mind ? '",
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                  onPressed: () {
-                    navigateTo(context, AddPostScreen());
-                  },
-                ),
-              ),
-            ),
-            Container(
-              width: 2.w,
-              height: 50.h,
-              color: Colors.grey,
-            ),
-            IconButton(
-              onPressed: () {
-                cubit.getPostImage();
-                navigateTo(context, AddPostScreen());
-              },
-              icon: Icon(
-                Icons.photo_library_outlined,
-                size: 30.sp,
-                color: cubit.isDark
-                    ? CupertinoColors.activeBlue
-                    : AppMainColors.whiteColor,
-              ),
-              splashRadius: 20.r,
-            ),
           ],
         ),
       ),
--- lib/shared/widget/create_post.dart
+++ lib/shared/widget/create_post.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sociality/pages/addPost/add_post_screen.dart';
 import 'package:sociality/shared/components/image_with_shimmer.dart';
 import 'package:sociality/shared/components/navigator.dart';
 import 'package:sociality/shared/cubit/socialCubit/social_cubit.dart';
-import 'package:sociality/shared/cubit/socialCubit/social_state.dart';
 import 'package:sociality/shared/styles/color.dart';
 
-class CreatePost extends StatelessWidget {
-  const CreatePost({
+class CreatePosts extends StatelessWidget {
+  const CreatePosts({
     super.key,
     required this.cubit,
   });
@@ -19,80 +17,73 @@ class CreatePost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<SocialCubit, SocialStates>(
-      listener: (context, state) {},
-      builder: (context, state) {
-        return Card(
-          clipBehavior: Clip.antiAliasWithSaveLayer,
-          elevation: 10,
-          margin: const EdgeInsets.all(10).r,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0).r,
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 25.r,
-                  child: ImageWithShimmer(
-                    imageUrl: cubit.userModel!.image,
-                    width: 100.w,
-                    height: 100.h,
+    return Card(
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      elevation: 10,
+      margin: const EdgeInsets.all(10).r,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0).r,
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 25.r,
+              child: ImageWithShimmer(
+                radius: 25.r,
+                imageUrl: cubit.userModel!.image,
+                width: 100.w,
+                height: 100.h,
+                boxFit: BoxFit.fill,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: 220.w,
+                height: 35.h,
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                margin: const EdgeInsets.all(10).r,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: AppMainColors.greyColor,
                   ),
+                  borderRadius: BorderRadius.circular(25).r,
                 ),
-                Expanded(
-                  child: Container(
-                    width: 220.w,
-                    height: 35.h,
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    margin: const EdgeInsets.all(10).r,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: AppMainColors.greyColor,
-                      ),
-                      borderRadius: BorderRadius.circular(25).r,
-                    ),
-                    child: TextButton(
-                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                      style: TextButton.styleFrom(
-                        backgroundColor:
-                            AppMainColors.greyColor.withOpacity(0.1),
-                      ),
-                      child: Text(
-                        "' What's on your mind ? '",
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      onPressed: () {
-                        navigateTo(
-                          context,
-                          AddPostScreen(),
-                        );
-                      },
-                    ),
+                child: TextButton(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  style: TextButton.styleFrom(
+                    backgroundColor: AppMainColors.greyColor.withOpacity(0.1),
                   ),
-                ),
-                Container(
-                  width: 2.w,
-                  height: 50.h,
-                  color: Colors.grey,
-                ),
-                IconButton(
+                  child: Text(
+                    "' What's on your mind ? '",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
                   onPressed: () {
-                    cubit.getPostImage();
                     navigateTo(context, AddPostScreen());
                   },
-                  icon: Icon(
-                    Icons.photo_library_outlined,
-                    size: 30.sp,
-                    color: cubit.isDark
-                        ? CupertinoColors.activeBlue
-                        : AppMainColors.whiteColor,
-                  ),
-                  splashRadius: 20.r,
                 ),
-              ],
+              ),
             ),
-          ),
-        );
-      },
+            Container(
+              width: 2.w,
+              height: 50.h,
+              color: Colors.grey,
+            ),
+            IconButton(
+              onPressed: () {
+                cubit.getPostImage();
+                navigateTo(context, AddPostScreen());
+              },
+              icon: Icon(
+                Icons.photo_library_outlined,
+                size: 30.sp,
+                color: cubit.isDark
+                    ? CupertinoColors.activeBlue
+                    : AppMainColors.whiteColor,
+              ),
+              splashRadius: 20.r,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
