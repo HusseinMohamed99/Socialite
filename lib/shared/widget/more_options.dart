69eef74635aea3e9ab9c28350696e675dd16f31f
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-08-20 03:36:50 +0300

    Changed the background color of the more options modal to be consistent with the app theme. Also added a new widget for displaying the items in the modal.

--- lib/shared/widget/more_options.dart
+++ lib/shared/widget/more_options.dart
@@ -3,196 +3,125 @@ import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sociality/model/post_model.dart';
 import 'package:sociality/pages/post/edit_post.dart';
-import 'package:sociality/shared/components/constants.dart';
 import 'package:sociality/shared/components/navigator.dart';
 import 'package:sociality/shared/cubit/socialCubit/social_cubit.dart';
+import 'package:sociality/shared/styles/color.dart';
 
 Future<dynamic> moreOption(BuildContext context, SocialCubit cubit,
     String postId, PostModel postModel) {
   return showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      shape: RoundedRectangleBorder(
-        borderRadius: const BorderRadius.vertical(
-          top: Radius.circular(30),
-        ).r,
-      ),
-      builder: (context) {
-        return Padding(
-          padding: const EdgeInsets.all(15.0).r,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              if (postModel.uId == cubit.userModel!.uId)
-                InkWell(
-                  onTap: () {
-                    navigateTo(
-                      context,
-                      EditPosts(
-                        postModel: postModel,
-                        postId: postId,
-                      ),
-                    );
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      left: 8.r,
-                      right: 8.r,
-                      top: 20.r,
-                      bottom: 0.r,
+    backgroundColor: cubit.isDark
+        ? AppMainColors.kittenWithColor
+        : AppColorsDark.primaryDarkColor,
+    context: context,
+    isScrollControlled: true,
+    shape: RoundedRectangleBorder(
+      borderRadius: const BorderRadius.vertical(top: Radius.circular(30)).r,
+    ),
+    builder: (context) {
+      return Padding(
+        padding: const EdgeInsets.all(15.0).r,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (postModel.uId == cubit.userModel!.uId)
+              ShowModalSheetItems(
+                function: () {
+                  navigateTo(
+                    context,
+                    EditPosts(
+                      postModel: postModel,
+                      postId: postId,
                     ),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.edit_location_outlined,
-                          color: cubit.isDark ? Colors.black : Colors.white,
-                          size: 30.sp,
-                        ),
-                        space(10.w, 0),
-                        Text(
-                          "Edit Post",
-                          style: TextStyle(
-                              color: cubit.isDark ? Colors.black : Colors.white,
-                              fontWeight: FontWeight.w600,
-                              fontSize: 20.sp),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              InkWell(
-                onTap: () {
-                  // cubit.savePost(
-                  //     postId: postId,
-                  //     date: DateTime.now(),
-                  //     userName: model.name,
-                  //     userId: model.uId,
-                  //     userImage: model.image,
-                  //     postText: model.text,
-                  //     postImage: model.postImage);
+                  );
                 },
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    left: 8.r,
-                    right: 8.r,
-                    top: 20.r,
-                    bottom: 0.r,
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.turned_in_not_sharp,
-                        color: cubit.isDark ? Colors.black : Colors.white,
-                        size: 30.sp,
-                      ),
-                      space(10.w, 0),
-                      Text(
-                        "Save Post",
-                        style: TextStyle(
-                          color: cubit.isDark ? Colors.black : Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 20.sp,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+                iconData: Icons.edit_location_outlined,
+                text: 'Edit Post',
               ),
-              if (postModel.postImage != '')
-                InkWell(
-                  onTap: () {
-                    cubit.saveToGallery(postModel.postImage!);
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      left: 8.r,
-                      right: 8.r,
-                      top: 20.r,
-                      bottom: 0.r,
-                    ),
-                    child: Row(
-                      children: [
-                        Icon(
-                          IconlyLight.download,
-                          color: cubit.isDark ? Colors.black : Colors.white,
-                          size: 30.sp,
-                        ),
-                        space(10.w, 0),
-                        Text(
-                          "Save Image",
-                          style: TextStyle(
-                            color: cubit.isDark ? Colors.black : Colors.white,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 20.sp,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    left: 8.r,
-                    right: 8.r,
-                    top: 20.r,
-                    bottom: 0.r,
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.share,
-                        color: cubit.isDark ? Colors.black : Colors.white,
-                        size: 30.sp,
-                      ),
-                      space(10.w, 0),
-                      Text(
-                        "Share",
-                        style: TextStyle(
-                          color: cubit.isDark ? Colors.black : Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 20.sp,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+            ShowModalSheetItems(
+              function: () {
+                // cubit.savePost(
+                //     postId: postId,
+                //     date: DateTime.now(),
+                //     userName: model.name,
+                //     userId: model.uId,
+                //     userImage: model.image,
+                //     postText: model.text,
+                //     postImage: model.postImage);
+              },
+              iconData: Icons.turned_in_not_sharp,
+              text: "Save Post",
+            ),
+            if (postModel.postImage != '')
+              ShowModalSheetItems(
+                function: () {
+                  cubit.saveToGallery(postModel.postImage!);
+                },
+                iconData: IconlyLight.download,
+                text: 'Saved Image',
               ),
-              if (postModel.uId == cubit.userModel!.uId)
-                InkWell(
-                  onTap: () {
-                    cubit.deletePost(postId);
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      left: 8.r,
-                      right: 8.r,
-                      top: 20.r,
-                      bottom: 0.r,
-                    ),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.delete,
-                          color: cubit.isDark ? Colors.black : Colors.white,
-                          size: 30.sp,
-                        ),
-                        space(10.w, 0),
-                        Text(
-                          "Delete Post",
-                          style: TextStyle(
-                            color: cubit.isDark ? Colors.black : Colors.white,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 20.sp,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-            ],
-          ),
-        );
-      });
+            ShowModalSheetItems(
+              function: () {},
+              iconData: Icons.share,
+              text: 'Share',
+            ),
+            if (postModel.uId == cubit.userModel!.uId)
+              ShowModalSheetItems(
+                function: () {
+                  cubit.deletePost(postId);
+                },
+                iconData: Icons.delete,
+                text: "Delete Post",
+              ),
+          ],
+        ),
+      );
+    },
+  );
+}
+
+class ShowModalSheetItems extends StatelessWidget {
+  const ShowModalSheetItems({
+    super.key,
+    required this.iconData,
+    required this.text,
+    required this.function,
+  });
+
+  final IconData iconData;
+  final String text;
+  final void Function()? function;
+  @override
+  Widget build(BuildContext context) {
+    SocialCubit cubit = SocialCubit.get(context);
+    return InkWell(
+      onTap: () {
+        function!();
+      },
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: 8.r,
+          right: 8.r,
+          top: 20.r,
+          bottom: 0.r,
+        ),
+        child: Row(
+          children: [
+            Icon(
+              iconData,
+              color: cubit.isDark
+                  ? AppMainColors.blackColor
+                  : AppMainColors.kittenWithColor,
+              size: 30.sp,
+            ),
+            SizedBox(width: 10.w),
+            Text(
+              text,
+              style: Theme.of(context).textTheme.titleLarge,
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
